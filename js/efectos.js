cdaf28adabce4a240a09db7f9c598a03a3686d63
Nolo-98 <twisterfast@gmail.com>
2021-06-25 20:01:23 -0500

    avance

--- js/efectos.js
+++ js/efectos.js
@@ -20,3 +20,38 @@ $(document).ready(function() {
              marginTop: '-52px'
          },1500);
      }
+
+     var acercaDE = $('#acerca-de').offset().top,
+    menu = $('#platillos').offset().top,
+    galeria = $('#galeria').offset().top,
+    ubicacion = $('#ubicacion').offset().top;
+
+    $('#btn-acerca-de').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 580
+        }, 500);
+    });
+
+    $('#btn-menu').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: menu
+        }, 500);
+    });
+    
+    $('#btn-galeria').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: galeria
+        }, 500);
+    });
+
+    $('#btn-ubicacion').on('click', function(e){
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: ubicacion
+        }, 500);
+    });
+
+});
